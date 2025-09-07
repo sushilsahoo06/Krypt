53952b3a63d5f5f1d69e404fe405c8158fd75c4c
sushilsahoo06 <sahoosushil456@gmail.com>
2025-09-07 12:54:49 +0530

    first commit

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,11 +1,12 @@
-require('@nomiclabs/hardhat-waffle');
+require("@nomicfoundation/hardhat-toolbox");
+
 
 module.exports = {
   solidity: '0.8.0',
   networks: {
-    ropsten: {
-      url: 'https://eth-ropsten.alchemyapi.io/v2/z4WpA8UKgqnwbTYmrZu15yCOiijBKaRv',
-      accounts: ['2f99db8cdb04655028eee1dc98230925202f6b3e010e43fad2883b4bea90a1a3'],
+    sepolia: {
+      url: 'https://eth-sepolia.g.alchemy.com/v2/NOQgDF8gkO6mnrwWW7NLp',
+      accounts: ['dbd1509619397f7df6cca74546bd14424856b52f466b5b94f4c21a24cf4e201b'],
     },
   },
 };
